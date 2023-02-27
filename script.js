b6e4534bd77d1d576d4f713c087d9056204cc5aa
Thomas Calle <thos.calle@gmail.com>
2023-02-27 02:22:15 -0500

    final script.js

--- script.js
+++ script.js
@@ -1,66 +1,68 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
-
-$(function () {
-  hourlyColor();
-  textEntry();
-
-  function updateTime() {
-    const dateElement = document.querySelector("#date");
-    const timeElement = document.querySelector("#time");
-    const currentDate = dayjs().format('dddd, MMMM D, YYYY');
-    const currentTime = dayjs().format('hh:mm:ss A');
-    dateElement.textContent = currentDate;
-    timeElement.textContent = currentTime;
-  }
-  
-  setInterval(updateTime, 1000);
-  
+  // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+  // the code isn't run until the browser has finished rendering all the elements
+  // in the html.
   const localeSettings = {};
   dayjs.locale(localeSettings);
-
-  function hourlyColor() {
+  // Wait until the DOM is fully loaded before executing the code inside the function.
+  $(function () {
+    // Get the current hour of the day using the dayjs library.
+    const currentHour = dayjs().format('H');
+  // The function below changes the color of each time block based on whether it's in the "past, present, or future" relative to the current hour.
+    function hourlyColor() {
+      $('.time-block').each(function() {
+        const blockHour = parseInt(this.id);
+        $(this).toggleClass('past', blockHour < currentHour);
+        $(this).toggleClass('present', blockHour === currentHour);
+        $(this).toggleClass('future', blockHour > currentHour);
+      });
+    }
+  // The  function below will save the user's input in a textarea to localStorage - only when the corresponding save button has been clicked.
+    function textEntry() {
+      $('.saveBtn').on('click', function() {
+        const key = $(this).parent().attr('id');
+        const value = $(this).siblings('.description').val();
+        localStorage.setItem(key, value);
+      });
+    }
+   // The function below will refresh the color of each time block based on whether it's in the past(grey), present(red), or future(green) relative to the current time. 
+    function refreshColor() {
+      $('.time-block').each(function() {
+        const blockHour = parseInt(this.id);
+        if (blockHour == currentHour) {
+          $(this).removeClass('past future').addClass('present');
+        } else if (blockHour < currentHour) {
+          $(this).removeClass('future present').addClass('past');
+        } else {
+          $(this).removeClass('past present').addClass('future');
+        }
+        if (currentHour > 17 || currentHour < 9) {
+          $(this).removeClass('past present').addClass('future');
+        }
+      });
+    }
+    // This will get the user input from the localStorage and set textarea values for each time block.
     $('.time-block').each(function() {
-      co\
-
-  // local storage 2
-  $(".saveBtn").on("click", function () {
-    var key = $(this).parent().attr("id"); // get the time-block id
-    var value = $(this).siblings(".description").val(); // get the user input
-    localStorage.setItem(key, value); // save to local storage
+      const key = $(this).attr('id');
+      const value = localStorage.getItem(key);
+      $(this).children('.description').val(value);
+    });
+  
+    // Please note: this is my favourtie part of the module - I absolutly love the display of current date and time especially 
+    // since the the time referesed every second - you can find this among the header of the page!
+    function updateTime() {
+      const dateElement = $('#date');
+      const timeElement = $('#time');
+      const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+      const currentTime = dayjs().format('hh:mm:ss A');
+      dateElement.text(currentDate);
+      timeElement.text(currentTime);
+    }
+    // Call the three main functions to set up the page.
+    hourlyColor();
+    textEntry();                
+    refreshColor();
+    // This will update the time once per second for the current time once per second using setInterval() 
+    // giving my beautiful diplay header a live time hours, minues and seconds coutner
+    setInterval(updateTime, 1000);
   });
-
-
-  // Initialize the jQuery File Upload plugin
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-  // Add code to display the current date in the header of the page
-  // Answer: Here lay the code to displaay the current date and time among the header of the page.
-  function updateTime() {
-    const dateElement = document.querySelector("#date");
-    const timeElement = document.querySelector("#time");
-    const currentDate = dayjs().format("dddd, MMMM D, YYYY");
-    const currentTime = dayjs().format("hh:mm:ss A");
-    dateElement.textContent = currentDate;
-    timeElement.textContent = currentTime;
-  }
-
-  setInterval(updateTime, 1000);
-});
